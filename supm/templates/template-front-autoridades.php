a7bbeb9272f040f917cb3695ada79d410ccb4ce8
Vicente Scópise <vscopise@gmail.com>
2020-05-23 10:05:58 -0300

    First commit

---
+++ supm/templates/template-front-autoridades.php
@@ -0,0 +1,89 @@
+<section class="hestia-testimonials autoridades" id="autoridades" data-sorder="hestia_testimonials">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-center hestia-testimonials-title-area">
+                <h5 class="description">AUTORIDADES</h5>
+            </div>
+        </div>
+        <div class="row title">
+            <div class="col-md-8 col-md-offset-2 text-center hestia-testimonials-title-area">
+                <h2 class="hestia-title">Comisión Directiva</h2>
+            </div>
+        </div>
+        <div class="hestia-testimonials-content">
+            <div class="row">
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Mág. Lic. Cecilia Durán</h4>
+                            <h6 class="category text-muted">Presidenta</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Lic. Mary Barros</h4>
+                            <h6 class="category text-muted">Vicepresidente</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Lic. Ana Bentancor</h4>
+                            <h6 class="category text-muted">Secretaria</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Lic. María Noel Silvariño</h4>
+                            <h6 class="category text-muted">Prosecretaria</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Lic. Mercedes Viera</h4>
+                            <h6 class="category text-muted">Tesorera</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row title">
+            <div class="col-md-8 col-md-offset-2 text-center hestia-testimonials-title-area">
+                <h2 class="hestia-title">Comisión Fiscal</h2>
+            </div>
+        </div>
+        <div class="hestia-testimonials-content">
+            <div class="row">
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Dr. Juan  J. Dapueto</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Lic. Carla Francolino</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-ms-6 col-sm-6 col-md-4">
+                    <div class="card card-testimonial card-plain">
+                        <div class="content">
+                            <h4 class="card-title">Mág. Lic. Psic. Ana Ibáñez</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
